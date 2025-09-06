521058317f0560a8a8ffbe35dd5be62394819fae
astorasgamesweb <astorasgamesshop@gmail.com>
2025-09-06 18:40:34 -0400

    Fix ProgressLogger import error

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Moon, Sun, Upload, Download, MessageCircle } from 'lucide-react';
 import { useDarkMode } from './hooks/useDarkMode';
 import CSVUploader from './components/CSVUploader';
-import { ProgressLogger } from './components/ProgressLogger';
+import ProgressLogger from './components/ProgressLogger';
 import GameProcessor from './components/GameProcessor';
 import DarkModeToggle from './components/DarkModeToggle';
 
